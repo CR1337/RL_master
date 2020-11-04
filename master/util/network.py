1977b1629827d93096c6cdb76d3698acb7b8b96a
CR1337 <captain_1@gmx.de>
2020-11-04 18:20:44 +0100

    fixed network address determination

--- master/util/network.py
+++ master/util/network.py
@@ -10,16 +10,23 @@ if os.name == 'nt':
 
 else:
 
-    import fcntl
-    import struct
+    # import fcntl
+    # import struct
+    import subprocess
+
+    # def _get_hostname():
+    #     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #     return socket.inet_ntoa(fcntl.ioctl(
+    #         s.fileno(),
+    #         0x8915,  # SIOCGIFADDR
+    #         struct.pack('256s', 'wlan0'[:15])
+    #     )[20:24])
 
     def get_hostname():
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(
-            s.fileno(),
-            0x8915,  # SIOCGIFADDR
-            struct.pack('256s', 'wlan0'[:15])
-        )[20:24])
+        result = subprocess.run(["ifconfig", "wlan0"], encoding='utf-8')
+        result_str = result.stdout
+        idx = result_str.find('inet ')
+        return result_str[idx:].split(' ')[1]
 
     def get_network_address():
         own_host = get_hostname()
