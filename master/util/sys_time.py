9e8b85fcad8f65108bae666b7039f43afd776648
CR1337 <captain_1@gmx.de>
2020-11-08 19:26:30 +0100

    added system time getter

--- master/util/sys_time.py
+++ master/util/sys_time.py
@@ -6,4 +6,8 @@ def set_system_time(time):
     subprocess.call(shlex.split("timedatectl set-ntp false"))
     subprocess.call(shlex.split(f"sudo date -s '{time}'"))
 
+
+def get_system_time():
+    datetime.now().replace(tzinfo=None).isoformat()
+
 # https://stackoverflow.com/questions/12081310/python-module-to-change-system-date-and-time
