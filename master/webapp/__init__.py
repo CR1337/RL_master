c58ad2d75bc8d9fca6f9bda124ecb50a2be17f20
CR1337 <captain_1@gmx.de>
2020-12-27 17:41:11 +0100

    print exceptions in routes

--- master/webapp/__init__.py
+++ master/webapp/__init__.py
@@ -2,6 +2,7 @@ from flask import make_response
 from flask_api import status
 from functools import wraps
 import sys
+import traceback
 
 
 def handle_exceptions(func):
@@ -10,11 +11,13 @@ def handle_exceptions(func):
         try:
             response = func(*args, **kwargs)
         except Exception:
-            exc_type, exc, _ = sys.exc_info()
+            exc_type, exc, tb = sys.exc_info()
             content = {
                 'exception_type': str(exc_type),
-                'exception_args': vars(exc)
+                'exception_args': vars(exc),
+                'traceback': traceback.extract_tb(tb).format()
             }
+            print(content)
             status_code = status.HTTP_400_BAD_REQUEST
             response = make_response((content, status_code))
         finally:
